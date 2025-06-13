182ef02b546d6345a855b9ac067e6223a417320c
fish891016 <fish891016@gmail.com>
2025-06-14 03:25:18 +0800

    fix: update server.js to correct latest version

--- server.js
+++ server.js
@@ -51,12 +51,15 @@ app.get('/instagram-followers', async (req, res) => {
 });
 
 async function fetchFromApify(username) {
-  const url = `https://api.apify.com/v2/acts/fish891016~instagram-followers-count-scraper/run-sync-get-dataset-items?token=${process.env.APIFY_API_KEY}`;
+  const url = `https://api.apify.com/v2/actor-tasks/fish891016~instagram-followers-count-scraper-task/run-sync-get-dataset-items?token=${process.env.APIFY_API_KEY}`;
+
   const payload = {
-    userId: username,
-    resultsLimit: 1,
-    includeFollowers: false,
-    includeFollowing: false
+    input: {
+      userId: username,
+      resultsLimit: 1,
+      includeFollowers: false,
+      includeFollowing: false
+    }
   };
 
   const response = await fetch(url, {
